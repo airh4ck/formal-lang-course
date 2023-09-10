4920cc9f2fc61647a919fff369574e86cddc7cf0
airh4ck <dudnikoff98@gmail.com>
2023-09-10 20:18:55 +0300

    Add graph utils

---
+++ project/graphs/graph_info.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+from typing import Any
+import cfpq_data
+
+
+@dataclass
+class GraphInfo:
+    vertices: int
+    edges: int
+    labels: set[Any]
+
+
+def get_graph_info(name: str) -> GraphInfo:
+    path = cfpq_data.download(name)
+    graph = cfpq_data.graph_from_csv(path)
+
+    return GraphInfo(
+        graph.number_of_nodes,
+        graph.number_of_edges,
+        set(cfpq_data.get_sorted_labels(graph)),
+    )
---
+++ project/graphs/utils.py
@@ -0,0 +1,13 @@
+import cfpq_data
+import networkx as nx
+
+
+def create_labeled_two_cycles_graph(
+    n: int, m: int, labels: tuple[str, str]
+) -> nx.MultiDiGraph:
+    return cfpq_data.labeled_two_cycles_graph(n, m, labels)
+
+
+def graph_to_dot(graph: nx.MultiDiGraph, output_path: str) -> None:
+    pydot_graph = nx.drawing.nx_pydot.to_pydot(graph)
+    pydot_graph.write_raw(output_path)
