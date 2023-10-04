3182a8607eae9064b9625743dc9960f42a3c17d3
Ilya Dudnikov <dudnikoff98@gmail.com>
2023-10-04 16:23:42 +0300

    Implement modified BFS based RPQ

--- project/rpq/rpq.py
+++ project/rpq/rpq.py
@@ -1,11 +1,10 @@
 from networkx import MultiDiGraph
 from pyformlang.regular_expression import Regex
-from typing import Set
+from typing import Set, Dict
 
 from project.automata.utils import graph_to_nfa, regex_to_dfa
 from project.automata.decomposed_fa import DecomposedFA
 import scipy.sparse as sp
-from loguru import logger
 
 
 def rpq(
@@ -110,7 +109,6 @@ def bfs_based_rpq(
     mask_matrix = set_start_verts(mask_matrix, start_states, regex_start_states)
 
     matrix_changed = True
-
     visited_matrix = mask_matrix.copy()
     while matrix_changed:
         new_matrix = sp.csr_matrix(mask_matrix.shape, dtype=bool)
@@ -129,4 +127,69 @@ def bfs_based_rpq(
     for row, col in zip(*extract_right_sub_matrix(visited_matrix).nonzero()):
         if row in regex_final_states:
             result.add(col)
+    return result
+
+
+def bfs_based_rpq_by_vertice(
+    graph: MultiDiGraph,
+    regex: Regex,
+    start_states: Set = set(),
+    final_states: Set = set(),
+) -> Dict:
+    graph_decomposed_fa = DecomposedFA.from_fa(
+        graph_to_nfa(graph, start_states, final_states)
+    )
+    graph_start_states = {
+        index
+        for state, index in graph_decomposed_fa.states_with_indices.items()
+        if state in graph_decomposed_fa.start_states
+    }
+
+    regex_decomposed_fa = DecomposedFA.from_fa(regex_to_dfa(regex))
+    regex_start_states = {
+        index
+        for state, index in regex_decomposed_fa.states_with_indices.items()
+        if state in regex_decomposed_fa.start_states
+    }
+    regex_final_states = {
+        index
+        for state, index in regex_decomposed_fa.states_with_indices.items()
+        if state in regex_decomposed_fa.final_states
+    }
+
+    direct_sum = regex_decomposed_fa.direct_sum(graph_decomposed_fa)
+
+    mask_matrices = {
+        state: create_masks(
+            regex_decomposed_fa.num_states, graph_decomposed_fa.num_states
+        )
+        for state in start_states
+    }
+    for col in start_states:
+        for row in regex_start_states:
+            mask_matrices[col][row, regex_decomposed_fa.num_states + col] = True
+
+    matrix_changed = True
+    visited_matrices = mask_matrices.copy()
+    while matrix_changed:
+        matrix_changed = False
+        for i in start_states:
+            new_matrix = sp.csr_matrix(mask_matrices[i].shape, dtype=bool)
+            for label in direct_sum:
+                new_matrix += transform_rows(mask_matrices[i] @ direct_sum[label])
+
+            prev_nnz = visited_matrices[i].nnz
+            visited_matrices[i] += new_matrix
+
+            if prev_nnz != visited_matrices[i].nnz:
+                matrix_changed = True
+                mask_matrices[i] = new_matrix
+
+    result = dict()
+    for i in start_states:
+        result[i] = set()
+        for row, col in zip(*extract_right_sub_matrix(visited_matrices[i]).nonzero()):
+            if row in regex_final_states:
+                result[i].add(col)
+
     return result
--- tests/automata/test_rpq.py
+++ tests/automata/test_rpq.py
@@ -3,7 +3,7 @@ from networkx import MultiDiGraph
 from pyformlang.regular_expression import Regex
 from pyformlang.finite_automaton import NondeterministicFiniteAutomaton
 
-from project.rpq.rpq import rpq, bfs_based_rpq
+from project.rpq.rpq import rpq, bfs_based_rpq, bfs_based_rpq_by_vertice
 
 
 def test_rpq_empty():
@@ -60,3 +60,32 @@ def test_bfs_based_rpq2():
     )
 
     assert bfs_based_rpq(graph, regex, {0, 1}, {2}) == {1, 2}
+
+
+def test_bfs_based_rpq_by_vertice1():
+    regex = Regex("b*a.b")
+    graph = MultiDiGraph(
+        [
+            (0, 1, {"label": "a"}),
+            (0, 3, {"label": "b"}),
+            (1, 2, {"label": "b"}),
+            (2, 0, {"label": "a"}),
+            (3, 0, {"label": "b"}),
+        ]
+    )
+
+    assert bfs_based_rpq_by_vertice(graph, regex, {0}, {}) == {0: {2}}
+
+
+def test_bfs_based_rpq_by_vertice2():
+    regex = Regex("(a|b)*b(a|b)")
+    graph = MultiDiGraph(
+        [
+            (0, 1, {"label": "a"}),
+            (1, 1, {"label": "b"}),
+            (1, 2, {"label": "b"}),
+            (2, 3, {"label": "c"}),
+        ]
+    )
+
+    assert bfs_based_rpq_by_vertice(graph, regex, {0, 1}, {2}) == {0: {1, 2}, 1: {1, 2}}
