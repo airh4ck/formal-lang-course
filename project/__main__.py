0c3c4a75cba20d81576eff69a49c994c4a80922e
Ilya Dudnikov <dudnikoff98@gmail.com>
2024-01-22 20:06:05 +0300

    Implement main

--- project/__main__.py
+++ project/__main__.py
@@ -1,1 +1,8 @@
-print("exec sources directory")
+import sys
+from project.language.interpreter import interpret
+
+if __name__ == "__main__":
+    with open(sys.argv[1], "r") as f:
+        code = f.read()
+
+    interpret(code)
